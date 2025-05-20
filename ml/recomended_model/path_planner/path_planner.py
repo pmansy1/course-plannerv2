f9449eb46ad99fdfb4dae80904e6ba0de9da7a3e
Macari <macarijoyner40@gmail.com>
2025-05-20 13:25:17 -0600

    macari's start

---
+++ ml/recomended_model/path_planner/path_planner.py
@@ -0,0 +1,24 @@
+import networkx as nx
+
+class PathPlanner:
+    def __init__(self):
+        self.graph = nx.DiGraph()
+
+    def add_prerequisites(self, course_pairs):
+        self.graph.add_edges_from(course_pairs)
+
+    def get_available_courses(self, completed):
+        available = []
+        for course in self.graph.nodes:
+            if course not in completed:
+                prereqs = set(self.graph.predecessors(course))
+                if prereqs.issubset(completed):
+                    available.append(course)
+        return available
+
+    def find_path(self, start_courses, end_course):
+        paths = []
+        for start in start_courses:
+            if nx.has_path(self.graph, start, end_course):
+                paths.append(nx.shortest_path(self.graph, start, end_course))
+        return paths
